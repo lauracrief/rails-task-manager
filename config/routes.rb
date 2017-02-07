21633a2ce5f9aa284466c2de7a4764442772f603
laura crief <crieflaura@gmail.com>
2017-02-07 17:25:37 +0100

    exercice rails done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tasks
+  #inventer le routing crud
+  #lire toutes les taches
+  #get "tasks" => "tasks#index"
+  #lire une tache précise
+  #get "tasks/id" => "tasks#show"
+  #creer une nouvelle tâche
+  #get "tasks/new" => "tasks#new"
+  #post "tasks" => "tasks#create"
+  #updater une nouvelle tâche
+  #get "tasks/:id/edit" => "tasks#edit"
+  #patch "tasks/:id" => "tasks#update"
+  #supprimer une tâche
+  #delete "tasks/:id" =>"tasks#destroy"
+  #delete verbe http mieux vaut ecrire destroy
+
+  root to: 'tasks#index'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+  Task.create(description: "cooking")
+  Task.create(description: "menage")
+  Task.create(description: "repassage")
+  Task.create(description: "peinture")
